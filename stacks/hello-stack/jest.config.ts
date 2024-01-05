3f85be9188188f56ae72133cde7aa07374ee1634
soohyun <soohyun@classting.com>
2024-01-05 16:10:52 +0900

    chore: hello-stack 생성
    - npx nx g serverless hello-stack

---
+++ stacks/hello-stack/jest.config.ts
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export default {
+  displayName: 'hello-stack',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  coverageDirectory: '../../coverage/stacks/hello-stack'
+};
